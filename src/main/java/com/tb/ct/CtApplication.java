29477acd1cec1b652de78dfea1a24b9bb12d402a
Chingiz <chingiz@MacBook-Air-Chingiz.local>
2024-12-14 16:10:18 +0100

    initial commit

---
+++ src/main/java/com/tb/ct/CtApplication.java
@@ -0,0 +1,12 @@
+package com.tb.ct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tb/ct/CtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tb.ct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
